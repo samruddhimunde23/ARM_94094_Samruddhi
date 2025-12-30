0a5f2df749e94f92842c0790806dae208169a858
samruddhimunde23 <samruddhimunde23@gmail.com>
2025-12-30 21:04:49 +0530

    Update main.c

--- Smart_Env_Monitor/MQ2_LED/Core/Src/main.c
+++ Smart_Env_Monitor/MQ2_LED/Core/Src/main.c
@@ -145,9 +145,6 @@ int main(void)
 	  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
 	  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
 	  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
-
-
-
   }
   HAL_Delay(500);
 
